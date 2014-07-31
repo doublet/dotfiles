a4ec71707192ac6edf8407e43148e37a678b1ea4
Thomas Toye <toye.thomas@gmail.com>
2014-07-31 22:16:02 +0200

    Install everything at once

--- deploy_debian.sh
+++ deploy_debian.sh
@@ -1,20 +1,17 @@
 #/bin/bash
 
+echo "Updating mirrorlist..."
 sudo apt-get update
-sudo apt-get install zsh
-chsh zsh
+echo "Installing all required packages..."
+sudo apt-get install zsh vim git
+echo "Setting zsh as default shell..."
+chsh -s zsh
 
 if [ -f ./README.md ]; then
   echo "README.md was found. Assuming you have cloned the repo in this directory."
   echo "NOTE: you might want to update this repo periodically. To do so:"
   echo "   $ git pull"
   echo ""
-  echo "Updating mirrorlist..."
-  sudo apt-get update
-  echo "Installing zsh..."
-  sudo apt-get install zsh -y
-  echo "Changing login shell to zsh..."
-  chsh -s zsh
   echo "Installing oh-my-zsh..."
   sudo apt-get install curl -y
   curl -L http://install.ohmyz.sh | sh
@@ -22,15 +19,7 @@ if [ -f ./README.md ]; then
   echo "Installed required packages. Now going to install dotfiles..."
   ./copy_headless.sh
 else
-    echo "No README.md found. Assuming you haven't cloned the repo."
-  echo "Checking for git..."
-  command -v git
-  GIT_NOT_INSTALLED=$?
-  if [ GIT_NOT_INSTALLED ];
-  then
-    echo "Git was not found. Installing..."
-    sudo apt-get install git -y
-  fi
+  echo "No README.md found. Assuming you haven't cloned the repo."
   echo "Cloning repo into ~/.dotfiles and continuing from there..."
   git clone https://github.com/doublet/dotfiles.git ~/.dotfiles
   cd ~/.dotfiles
